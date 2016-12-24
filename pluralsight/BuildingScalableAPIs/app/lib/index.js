e73ed23f0e72ec83b1e860ad0d86916462c87109
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2016-12-24 06:07:03 -0500

    Module loading system not working

--- pluralsight/BuildingScalableAPIs/app/lib/index.js
+++ pluralsight/BuildingScalableAPIs/app/lib/index.js
@@ -1,2 +1,14 @@
 const Env  = require('./util');
 console.log(`Running in ${Env.nodeEnv} mode...`);
+
+
+// Read the query from the command line arguments
+const query = process.argv[2];
+
+const ncSchema = require('../schema');
+const { graphql } = require('graphql');
+
+//Excute the query against the dfines server schema
+graphql(ncSchema,query).then(result => {
+  console.log(result);
+});
