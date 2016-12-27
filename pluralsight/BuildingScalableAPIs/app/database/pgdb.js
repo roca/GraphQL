f866c5042d4828eda0ee0377517f42c9afab400c
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2016-12-27 05:44:39 -0500

    Learn how to do a subQuery

--- pluralsight/BuildingScalableAPIs/app/database/pgdb.js
+++ pluralsight/BuildingScalableAPIs/app/database/pgdb.js
@@ -7,6 +7,12 @@ module.exports = pgPool => {
                 select * from users
                 where api_key = $1
             `,[apiKey]).then( res => humps.camelizeKeys(res.rows[0]));
+        },
+        getContests(user) {
+            return pgPool.query(`
+            select * from contests 
+            where created_by = $1
+            `,[user.id]).then(res => humps.camelizeKeys(res.rows));
         }
     }
 }
--- pluralsight/BuildingScalableAPIs/app/schema/types/me.js
+++ pluralsight/BuildingScalableAPIs/app/schema/types/me.js
@@ -2,11 +2,15 @@ const {
     GraphQLObjectType,
     GraphQLID,
     GraphQLString,
-    GraphQLNonNull
+    GraphQLNonNull,
+    GraphQLList
 } = require('graphql');
 
+const pgdb = require('../../database/pgdb');
+const ContestType = require('./contest');
+
  module.exports = new GraphQLObjectType({
-    name: 'meType',
+    name: 'MeType',
     fields: {
         id: { type: GraphQLID},
         firstName: {type: GraphQLString},
@@ -16,6 +20,15 @@ const {
             resolve: obj => `${obj.firstName} ${obj.lastName}`
         },
         email: { type: new GraphQLNonNull(GraphQLString)},
-        createdAt: {type: GraphQLString}
+        createdAt: {type: GraphQLString},
+        contests: {
+            type: new GraphQLList(ContestType) ,
+            resolve: (obj, args, { pgPool }) => {
+                // Read user information from database
+                // using args.key as the api key
+                // using pgPool...
+                return pgdb(pgPool).getContests(obj);
+            }
+        }
     }
 })
