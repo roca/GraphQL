f8391fff533884d621ab43931759f26715f9aa24
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2017-01-14 06:10:36 -0500

    Using webpack in build now

---
+++ pluralsight/BuildingReactApp/rgrjs/public/js/app.js
@@ -0,0 +1,10 @@
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+var Hello = React.createClass({
+    render: function() {
+        return React.createElement("h3", null, "Hello WebPack!");
+    }
+});
+
+ReactDOM.render(React.createElement(Hello), document.getElementById('react'));
