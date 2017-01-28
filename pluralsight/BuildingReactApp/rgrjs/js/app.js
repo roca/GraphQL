aa37975f1b2d7c5a4c805a3e6696fd7cf6b8638e
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2017-01-28 05:52:48 -0500

    Can't load plugin

--- pluralsight/BuildingReactApp/rgrjs/js/app.js
+++ pluralsight/BuildingReactApp/rgrjs/js/app.js
@@ -1,7 +1,18 @@
 import React from "react";
 import  ReactDOM from "react-dom";
+import Relay from "react-relay";
 
 import Main from "./components/Main";
 
 
-ReactDOM.render(<Main />, document.getElementById('react'))
+ReactDOM.render(<Main />, document.getElementById('react'));
+
+console.log(
+    Relay.QL`
+        {
+        links {
+            title
+        }
+        }
+    `
+);
--- pluralsight/BuildingReactApp/rgrjs/server.js
+++ pluralsight/BuildingReactApp/rgrjs/server.js
@@ -1,6 +1,9 @@
+import fs from 'fs';
 import express from 'express';
-import schema from './data/schema';
+import Schema from './data/schema';
 import GraphQLHTTP from 'express-graphql';
+import { graphql } from 'graphql';
+import { introspectionQuery } from 'graphql/utilities';
 
 import {MongoClient} from 'mongodb';
 
@@ -9,12 +12,21 @@ app.use(express.static('public'));
 
 (async () => {
  let db = await MongoClient.connect(process.env.MONGO_URL);
+ let schema = Schema(db);
  
     app.use('/graphql', GraphQLHTTP({
-        schema: schema(db),
+        schema,
         graphiql: true
     }));  
 
     app.listen(3000,() => console.log('Listening on port 3000'));
+
+    // Generate schema.json
+    let json = await graphql(schema, introspectionQuery);
+    fs.writeFile('./data/schema.json', JSON.stringify(json, null, 2), err => {
+        if (err) throw err;
+
+        console.log("JSON schema created");
+    });
 })();
 
