1bad66c7aac9aebf1d9630a857222d91d2c576b0
Romel Campbell <romel.campbell@regeneron.com>
2017-02-14 07:52:32 -0500

    Adds list array type. Now using graphiql webserver

--- egghead/graphql/getting-started/index.js
+++ egghead/graphql/getting-started/index.js
@@ -1,13 +1,26 @@
 'use strict';
 
+const express = require('express');
+
+const graphqlHTTP = require('express-graphql');
+
 const { graphql, buildSchema } = require('graphql');
 
+const PORT = process.env.PORT || 3000;
+
+const server = express();
+
 const schema = buildSchema(`
-    type Query {
+
+    type Video {
         id: ID,
         title: String,
         duration: Int,
         watched: Boolean
+    }
+    type Query {
+        video: Video,
+        videos: [ Video ]
     }
 
     type Schema {
@@ -15,23 +28,39 @@ const schema = buildSchema(`
     }
 `);
 
-const resolvers = {
-    id: () => 1,
-    title: () => 'bar',
-    duration: () => 180,
-    watched: () => true
+const videoA = {
+    id: 'a',
+    title: 'Create a GraphQL Schema',
+    duration: 120,
+    watched: true
+};
+const videoB = {
+    id: 'b',
+    title: 'Ember.js CLI',
+    duration: 120,
+    watched: false
 };
 
+const videos = [videoA, videoB];
 
-const query = `
-    query myFirstQuery {
-       id,
-       title,
-       duration,
-       watched
-    }
-`
+const resolvers = {
+    video: () => {
+        return {
+            id: 1,
+            title: 'Foo',
+            duration: 180,
+            watched: true
+        };
+    },
+    videos: () => videos
+};
 
-graphql(schema,query,resolvers)
-.then((result) => console.log(result))
-.catch((error) => console.log(error));
+server.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true,
+    rootValue: resolvers
+}));
+
+server.listen(PORT, () => {
+    console.log(`Listening on http://localhost:${PORT}`);
+});
