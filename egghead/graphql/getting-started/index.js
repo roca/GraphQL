0bd2c09fe95905088d451deef892e08e42899f41
Romel Campbell <romel.campbell@regeneron.com>
2017-02-16 08:50:14 -0500

    Creates a mutation for adding vedos

--- egghead/graphql/getting-started/index.js
+++ egghead/graphql/getting-started/index.js
@@ -7,13 +7,15 @@ const graphqlHTTP = require('express-graphql');
 const { 
     GraphQLSchema,
     GraphQLObjectType,
+    GraphQLList,
+    GraphQLNonNull,
     GraphQLID,
     GraphQLString,
     GraphQLInt,
     GraphQLBoolean
 } = require('graphql');
 
-const { getVideoById } = require('./src/data');
+const { getVideoById, getVideos, createVideo } = require('./src/data');
 
 const PORT = process.env.PORT || 3000;
 
@@ -34,19 +36,39 @@ const queryType = new GraphQLObjectType({
     name: 'QueryType',
     description: 'The root query type.',
     fields: {
+        videos: {
+            type: new GraphQLList(VideoType),
+            resolve: getVideos
+        },
         video: { 
             type: VideoType,
             args: {
-                id: { type: GraphQLID, description: 'The id of the video.'  }
+                id: { type: new GraphQLNonNull(GraphQLID), description: 'The id of the video.'  }
             },
             resolve: (_, args) => getVideoById(args.id)
         }
     }
-})
+});
+
+const mutationType = new GraphQLObjectType({
+    name: 'MutationType',
+    description: 'The root mutation type.',
+    fields: {
+        createVideos: {
+            type: VideoType,
+            args: {
+                title: { type: new GraphQLNonNull(GraphQLString), description: 'The title of the video.' },
+                duration: { type: new GraphQLNonNull(GraphQLInt), description: 'The duration of the video (in seconds).'  },
+                released: { type: new GraphQLNonNull(GraphQLBoolean), description: 'Whether or not the is released.'  }
+            },
+            resolve: (_, args) => createVideo(args)
+        }
+    }
+});
 
 const schema = new GraphQLSchema({
-    query: queryType
-    //mutation:
+    query: queryType,
+    mutation: mutationType
     //subscriptiton:
 });
 
