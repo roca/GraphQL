059e2616feb0ca4f2449d58c8e0090c84048124f
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2017-02-25 20:57:05 -0500

    app waiting for mongo to start correctly now

--- books/graphql-relay-samer-buna/app/index.js
+++ books/graphql-relay-samer-buna/app/index.js
@@ -1,34 +1,26 @@
-
-const { graphql } = require('graphql');
-const readline = require('readline');
 const  { MongoClient }  = require('mongodb');
-const assert = require('assert');
+const graphqlHTTP = require('express-graphql');
+const express = require('express');
 
 const { nodeEnv }  = require('./lib/util');
 const mongoConfig = require('./config/mongo')[nodeEnv];
-const mySchema = require('./schema/main');
+const Schema = require('./schema/main');
 
-const graphqlHTTP = require('express-graphql');
-const express = require('express');
 const app = express();
 
 console.log('nodeEnv: ' + nodeEnv);
 
-const rli = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+(async () => {
 
-MongoClient.connect(mongoConfig.url, (err, db) => {
-  assert.equal(null, err);
+   let db = await MongoClient.connect(mongoConfig.url); 
+   let schema = Schema(db);
 
-  console.log('Connected to MongoDB server')
+   console.log('Connected to MongoDB server');
 
     app.use('/graphql', graphqlHTTP({
-        schema: mySchema,
-        context: { db }
+        schema
     }));
 
     app.listen(3000, () => console.log('Runninng Express.js on port 3000'));
 
-});
+})();
