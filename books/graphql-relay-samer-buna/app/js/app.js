6aa62f26178d3233a84307c82f9bbd07f3b61fbb
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2017-03-12 07:20:18 -0400

    App rending quotes using react

--- books/graphql-relay-samer-buna/app/js/app.js
+++ books/graphql-relay-samer-buna/app/js/app.js
@@ -1,6 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Quote from './quote';
+
+class QuoteLibrary extends React.Component{
+    state = { allQuotes: [] };
+
+    componentDidMount() {
+        fetch(`/graphql?query={
+            allQuotes{
+                id,
+                text,
+                author
+            }
+        }`)
+        .then(response => response.json())
+        .then(json => this.setState(json.data))
+        .catch(ex => console.error(ex));
+    }
+
+    render() {
+        return (
+            <div className="quotes-list">
+                {this.state.allQuotes.map(quote => <Quote key={quote.id} quote={quote} />)}
+            </div>
+        )
+    }
+}
+
 class App extends React.Component {
     static defaultProps = {
         greeting: 'Hello'
@@ -16,7 +43,7 @@ class App extends React.Component {
 
 }
 
-ReactDOM.render(<App />, document.getElementById('react'));
+ReactDOM.render(<QuoteLibrary />, document.getElementById('react'));
 
 
 
