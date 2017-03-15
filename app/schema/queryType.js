544ffe159876fb3641babbf475eb8c340e2419e4
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2017-03-15 03:56:14 -0400

    Add a QuotesLibraryType

--- app/schema/queryType.js
+++ app/schema/queryType.js
@@ -10,13 +10,26 @@ const { QuoteType } = require('./types/quoteType');
 
 let QueryType = (db) => {
 
-    const queryType = new GraphQLObjectType({
-        name: 'RootQuery',
+    const QuotesLibraryType = new GraphQLObjectType({
+        name: 'QuotesLibrary',
         fields: {
             allQuotes: {
                 type: new GraphQLList(QuoteType),
                 description: 'A list of the quotes in the database',
                 resolve: (_, args) => db.collection('quotes').find().toArray()
+            }
+        }
+    });
+
+    const quotesLibrary = {};
+
+    const queryType = new GraphQLObjectType({
+        name: 'RootQuery',
+        fields: {
+            allQuotes: {
+                type: QuotesLibraryType,
+                description: 'The Quotes Library',
+                resolve: (_, args) => quotesLibrary
             },
             usersCount: {
                 description: 'Total number of users in the database',
