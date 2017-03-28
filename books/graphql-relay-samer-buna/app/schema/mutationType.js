40583b5008cfb9f3faa15cd7c76f50b4aade8eb7
Romel Campbell <romel.campbell@regeneron.com>
2017-03-28 03:57:58 -0400

    Adds thumbsUp Mutation

--- books/graphql-relay-samer-buna/app/schema/mutationType.js
+++ books/graphql-relay-samer-buna/app/schema/mutationType.js
@@ -1,17 +1,49 @@
 'use strict';
 
+const { ObjectID } = require('mongodb');
+
 const {   
     GraphQLObjectType,
     GraphQLString
 } = require('graphql')
 
+const {
+    fromGlobalId,
+    mutationWithClientMutationId
+} = require('graphql-relay');
+
 
 const { appendLinePromise } = require('../lib/util');
+const { QuoteType } = require('./types/quoteType');
+
+
+const thumbsUpMutation = mutationWithClientMutationId({
+    name: 'ThumbsUpMutation',
+    inputFields: {
+        quoteId: {type: GraphQLString}
+    },
+    outputFields: {
+        quote: {
+            type: QuoteType,
+            resolve: obj => obj
+        }
+    },
+    mutateAndGetPayload: (params, { db }) => {
+        const { id } = fromGlobalId(params.quoteId);
+        return Promise.resolve(
+            db.collection('quotes').updateOne(
+                {_id: ObjectID(id)},
+                { $inc: { likesCount: 1}}
+            )
+        ).then(result => db.collection('quotes').fondOne(ObjectID(id)));
+    }
+});
 
 
 const MutationType = new GraphQLObjectType({
     name: 'RootMutation',
     fields: {
+        thumbsUp: thumbsUpMutation,
         addQuote: {
             type: GraphQLString,
             args: {
