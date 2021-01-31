73ddbd0c22512ce7db3167be1d42ee57e392fcf2
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2021-01-31 13:13:32 -0500

    01312021/13:12: SignIn page working

--- udemy/ReactAndAmplify/family-helper/src/App.js
+++ udemy/ReactAndAmplify/family-helper/src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Amplify from 'aws-amplify';
+import awsConfig from './aws-exports';
+import {AmplifyAuthenticator, AmplifySignOut} from '@aws-amplify/ui-react';
+
+Amplify.configure(awsConfig);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Amplify
-        </a>
-      </header>
-    </div>
+    <AmplifyAuthenticator>
+      <div className="App">
+        <h1> Welcome to Amplify</h1>
+        <AmplifySignOut />
+      </div>
+    </AmplifyAuthenticator>
   );
 }
 
