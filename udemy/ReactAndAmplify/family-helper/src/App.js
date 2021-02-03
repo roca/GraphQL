7006294758c5a82fb56bd28bf7cf002885227211
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2021-02-03 05:23:20 -0500

    Retriveing data from AppSync server

--- udemy/ReactAndAmplify/family-helper/src/App.js
+++ udemy/ReactAndAmplify/family-helper/src/App.js
@@ -1,16 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
-import Amplify from 'aws-amplify';
+import Amplify, {API, graphqlOperation} from 'aws-amplify';
 import awsConfig from './aws-exports';
 import {AmplifyAuthenticator, AmplifySignOut} from '@aws-amplify/ui-react';
+import {listLists} from './graphql/queries';
+import { useEffect, useState } from 'react';
 
 Amplify.configure(awsConfig);
 
 function App() {
+  const [list, setList] = useState([]);
+  async function fetchList() {
+    const { data } = await API.graphql(graphqlOperation(listLists));
+    setList(data.listLists.items);
+    console.log(data);
+  }
+  useEffect(() => {
+     fetchList();
+  }, []);
   return (
     <AmplifyAuthenticator>
       <div className="App">
         <h1> Welcome to Amplify</h1>
+        <ul>
+         {list.map(item =>  <li key={item.id} >{item.title}</li> )}
+        </ul>
         <AmplifySignOut />
       </div>
     </AmplifyAuthenticator>
