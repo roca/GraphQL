69ca15e62b92fb1b93958c19801edb8cf636172a
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2021-02-07 05:54:56 -0500

    Modal has input fields

--- udemy/ReactAndAmplify/family-helper/src/App.js
+++ udemy/ReactAndAmplify/family-helper/src/App.js
@@ -5,7 +5,7 @@ import {AmplifyAuthenticator, AmplifySignOut} from '@aws-amplify/ui-react';
 import {listLists} from './graphql/queries';
 import { useEffect, useState } from 'react';
 import 'semantic-ui-css/semantic.min.css';
-import { Button, Container, Icon, Modal } from 'semantic-ui-react';
+import { Button, Container, Icon, Modal, Form } from 'semantic-ui-react';
 
 import MainHeder from './components/headers/MainHeader';
 
@@ -15,6 +15,8 @@ Amplify.configure(awsConfig);
 
 function App() {
   const [lists, setLists] = useState([]);
+  const [isModalOpen, setISModalOpen] = useState(true);
+  
   async function fetchList() {
     const { data } = await API.graphql(graphqlOperation(listLists));
     setLists(data.listLists.items);
@@ -23,13 +25,18 @@ function App() {
   useEffect(() => {
      fetchList();
   }, []);
+
+  function toggleModal(shouldOpen) {
+   setISModalOpen(shouldOpen);
+  }
+
   return (
     <AmplifyAuthenticator>
-      <AmplifySignOut />
-      <Button className="floatingButton">
-        <Icon name="plus" className="floatingButton_icon"/>
-      </Button>
-      <Container>
+      <Container style={{height: '100vh'}}>
+        <AmplifySignOut />
+        <Button className="floatingButton" onClick={() =>toggleModal(true)}>
+          <Icon name="plus" className="floatingButton_icon"/>
+        </Button>
         <div className="App">
           <MainHeder />
           <ul>
@@ -37,6 +44,26 @@ function App() {
           </ul>
         </div>
       </Container>
+      <Modal open={isModalOpen} dimmer="blurring">
+        <Modal.Header>Create your list</Modal.Header>
+        <Modal.Content>
+          <Form>
+            <Form.Input 
+              error={true ? false : {content: "please add a name to your list"}}
+              label="Title" 
+              placeholder="My pretty list"
+              ></Form.Input>
+            <Form.TextArea 
+              label="Description"
+              placeholder="Things that my pretty list is about"
+              ></Form.TextArea>
+          </Form>
+        </Modal.Content>
+        <Modal.Actions>
+          <Button negative onClick={() =>toggleModal(false)}>Cancel</Button>
+          <Button positive onClick={() =>toggleModal(false)}>Save</Button>
+        </Modal.Actions>
+      </Modal>
     </AmplifyAuthenticator>
   );
 }
