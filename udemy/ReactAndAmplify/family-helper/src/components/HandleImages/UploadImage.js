3976ef7313ec16115c90c5222a7421ecb728a515
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2021-02-19 05:18:21 -0500

    App uses amplify to put images on s3

--- udemy/ReactAndAmplify/family-helper/src/components/HandleImages/UploadImage.js
+++ udemy/ReactAndAmplify/family-helper/src/components/HandleImages/UploadImage.js
@@ -1,13 +1,33 @@
-import React from 'react'
-import { useRef, useState } from 'react';
-import { Header, Image, Button } from 'semantic-ui-react'
+import React from 'react';
+import { Storage } from 'aws-amplify';
+import { useEffect, useRef, useState } from 'react';
+import { Header, Image, Button } from 'semantic-ui-react';
 
 function UploadImage() {
     const inputRef = useRef();
     const [image, setImage] = useState('https://react.semantic-ui.com/images/wireframe/image.png');
+    const [fileName, setFileName] = useState();
 
-    function handleInputChange() {
-        
+    useEffect(() => {
+       if(!fileName) return
+       const [file, extension] = fileName.name.split(".");
+       const mimeType = fileName.type;
+       const key = `images/lists/${file}.${extension}`
+       const result = Storage.put(key, fileName,{
+           contentType: mimeType,
+           metadata: {
+               app: 'family helper'
+           }
+       });
+       console.log(result);
+    }, [fileName])
+
+    function handleInputChange(e) {
+        const fileToUpload = e.target.files[0];
+        if (!fileToUpload) return;
+        const fileSampleUrl = URL.createObjectURL(fileToUpload);
+        setImage(fileSampleUrl);
+        setFileName(fileToUpload);
     }
     
     return (
